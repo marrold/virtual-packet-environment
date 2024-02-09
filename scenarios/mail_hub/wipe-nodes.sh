3eba669bcc180a37e3499f0166f7fbcf8e3f0fdf
Matthew Harrold <github@marrold.co.uk>
2024-02-09 21:05:22 +0000

    latest

---
+++ scenarios/mail_hub/wipe-nodes.sh
@@ -0,0 +1,7 @@
+echo -n "" > GB7CIP/BPQNODES.dat
+echo -n "" > GB7OUK-10/BPQNODES.dat
+echo -n "" > GB7RAN/BPQNODES.dat
+echo -n "" > GB7OUK/BPQNODES.dat
+echo -n "" > GB7RDG/BPQNODES.dat
+echo -n "" > G7BCS/BPQNODES.dat
+echo -n "" > GB7IOW/BPQNODES.dat
